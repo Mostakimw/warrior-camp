3557c1eee8d250f4c62d0cefe6fc0b0965ce5392
Mostakimw <scientist.expert@gmail.com>
2023-06-11 02:38:51 +0600

    banner all done title and desc and pic updated

--- src/pages/Homepage/Banner/Banner.jsx
+++ src/pages/Homepage/Banner/Banner.jsx
@@ -16,41 +16,61 @@ const Banner = () => {
         slidesPerView={1}
       >
         <SwiperSlide>
-          <div className="relative h-[80vh] w-full bg-gradient-to-r">
+          <div className="relative h-[80vh] w-full bg-gradient-to-r ">
             <img
-              src="https://i.ibb.co/j51RzV7/banner-1.jpg"
+              src="https://i.ibb.co/xL5T31s/group-boys-girl-fighting-aikido-training-martial-arts-school-healthy-lifestyle-sports-concept-1.jpg"
               className="object-cover h-full w-full"
               alt=""
             />
-            <div className="absolute inset-0 flex flex-col justify-center items-center">
-              <h3 className="text-white text-2xl font-bold">Title</h3>
-              <p className="text-white text-lg">Description</p>
+            <div className="absolute inset-0 flex flex-col justify-center items-center bg-black bg-opacity-50 text-center ">
+              <h3 className="text-white text-4xl font-bold section-title">
+                Discover the Way of Martial Arts
+              </h3>
+              <p className="text-gray-100 text-lg max-w-xl mt-3">
+                Embark on a journey of self-discovery through martial arts. Join
+                our Summer Camp Learning School to cultivate discipline, build
+                strength, and foster personal growth. Start your martial arts
+                adventure now!
+              </p>
             </div>
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div className="relative h-[80vh] w-full bg-gradient-to-r">
+          <div className="relative h-[80vh] w-full bg-gradient-to-r ">
             <img
-              src="https://i.ibb.co/j51RzV7/banner-1.jpg"
+              src="https://i.ibb.co/1mxM3tj/pexels-rdne-stock-project-7045594.jpg"
               className="object-cover h-full w-full"
               alt=""
             />
-            <div className="absolute inset-0 flex flex-col justify-center items-center">
-              <h3 className="text-white text-2xl font-bold">Title</h3>
-              <p className="text-white text-lg">Description</p>
+            <div className="absolute inset-0 flex flex-col justify-center items-center bg-black bg-opacity-50 text-center ">
+              <h3 className="text-white text-4xl font-bold section-title">
+                Empowering Minds and Bodies
+              </h3>
+              <p className="text-gray-100 text-lg max-w-xl mt-3">
+                Experience the power of martial arts at our Summer Camp Learning
+                School. Develop mental fortitude, enhance physical fitness, and
+                boost self-confidence. Join our community and empower yourself
+                today!
+              </p>
             </div>
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div className="relative h-[80vh] w-full bg-gradient-to-r">
+          <div className="relative h-[80vh] w-full bg-gradient-to-r ">
             <img
-              src="https://i.ibb.co/j51RzV7/banner-1.jpg"
+              src="https://i.ibb.co/Sc74RSt/active-sporty-girl-kimono-kicking-with-leg-studio.jpg"
               className="object-cover h-full w-full"
               alt=""
             />
-            <div className="absolute inset-0 flex flex-col justify-center items-center">
-              <h3 className="text-white text-2xl font-bold">Title</h3>
-              <p className="text-white text-lg">Description</p>
+            <div className="absolute inset-0 flex flex-col justify-center items-center bg-black bg-opacity-50 text-center ">
+              <h3 className="text-white text-4xl font-bold section-title">
+                Ignite Your Inner Warrior
+              </h3>
+              <p className="text-gray-100 text-lg max-w-xl mt-3">
+                Unleash your inner warrior at our Summer Camp Learning School.
+                Learn martial arts, develop resilience, and unlock your full
+                potential. Join us on this transformative journey today!
+              </p>
             </div>
           </div>
         </SwiperSlide>
