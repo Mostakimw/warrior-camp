b789d717ca8200056003301414f396826234268f
Mostakimw <scientist.expert@gmail.com>
2023-06-12 14:45:07 +0600

    firebase deploys

--- src/hooks/useAdmin.jsx
+++ src/hooks/useAdmin.jsx
@@ -5,6 +5,7 @@ import useAxiosSecure from "./useAxiosSecure";
 const useAdmin = () => {
   const { user, loading } = useAuth();
   const [axiosSecure] = useAxiosSecure();
+
   const { data: isAdmin, isLoading: isAdminLoading } = useQuery({
     queryKey: ["isAdmin", user?.email],
     enabled: !loading,
